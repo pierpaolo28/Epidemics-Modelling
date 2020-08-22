e861baa14bbcd875e20e3e73ceb636091ccef708
Pier Paolo <ppi1u16@soton.ac.uk>
2020-08-22 13:12:30 +0100

    A/B Testing

--- src/pages/testing.py
+++ src/pages/testing.py
@@ -8,8 +8,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import pages.home
 import csv
-from support.utilities import world_map, world_plot, stats
-from support.base_models import SIR_sim, SIR_plot, SEIR_sim, SEIR_plot
+import scipy.stats as scs
 
 
 def write():
@@ -18,9 +17,13 @@ def write():
         st.markdown('## Live Feedback A/B Testing')
         st.markdown(
             '''
-            In this research study two different type of models have been examined: Agent Based Modelling and Compartmental Modelling. Comparmental Modelling 
-            represents the most traditional way to model epidemical developments (Control Group), while Agent Based Modelling offers an alternative view to how to approach this 
-            type of problem (Treatment Group). Which of the two approaches do you think would make you feel most sure/confortable to make a decision about possible restrictions to apply?
+            In this research study two different type of models have been examined: Agent Based Modelling and Compartmental Modelling. Compartmental Modelling 
+            represents the most traditional way (and gold standard) to model epidemical developments (Control Group), while Agent Based Modelling offers an alternative view to how to approach this 
+            type of problem (Treatment Group). 
+            - **Compartmental Modelling:** SIR and SEIR Modelling, Advanced SEIR Modelling, Vaccination Modelling, Coronavirus Modelling.
+            - **Agent Based Modelling:** Population Modelling, Track and Trace, Central Hubs, Finance Simulation.
+
+            Which of the two approaches do you think would make you feel most confortable to make a decision about possible interventions to apply (aiding your decision making)?
             '''
         )
 
@@ -30,7 +33,7 @@ def write():
             with open('src/pages/record.csv', 'a') as fd:
                 writer = csv.writer(fd)
                 writer.writerow([])
-                writer.writerow(['1', '0', '0'])
+                writer.writerow(['1', '0'])
             data = pd.read_csv('src/pages/record.csv')
             last_record = list(data.sum(axis=0))
             st.write(last_record[0])
@@ -38,22 +41,66 @@ def write():
             with open('src/pages/record.csv', 'a') as fd:
                 writer = csv.writer(fd)
                 writer.writerow([])
-                writer.writerow(['0', '1', '0'])
+                writer.writerow(['0', '1'])
             data = pd.read_csv('src/pages/record.csv')
             last_record = list(data.sum(axis=0))
             st.write(last_record[1])
-        if st.button('Neither'):
-            with open('src/pages/record.csv', 'a') as fd:
-                writer = csv.writer(fd)
-                writer.writerow([])
-                writer.writerow(['0', '0', '1'])
-            data = pd.read_csv('src/pages/record.csv')
-            last_record = list(data.sum(axis=0))
-            st.write(last_record[2])
-        st.write("Sample Size: ", sum(last_record))
+        st.write("Sample Size (logged responses): ",
+                 round(sum(last_record), 3))
 
         c_a = last_record[0]/sum(last_record)
         c_b = last_record[1]/sum(last_record)
 
-        st.write("Conversion Rate for Compartmental Modelling: ", c_a)
-        st.write("Conversion Rate for Agent Based Modelling: ", c_b)
+        cr_uplift = (c_b - c_a) / c_a
+        se_a, se_b = np.sqrt((c_a * (1 - c_a)) / sum(last_record)
+                             ), np.sqrt((c_b * (1 - c_b)) / sum(last_record))
+        se_diff = np.sqrt(se_a**2 + se_b**2)
+        z_score = (c_b - c_a) / se_diff
+        p_value = 1 - scs.norm(0, 1).cdf(z_score)
+
+        sides = st.radio("Type of Hypotesys", ('One Sided', 'Two Sided'))
+        if sides == 'One Sided':
+            sided = 0
+        else:
+            sided = 1
+
+        interval = st.slider("Required Confidence: ",
+                             min_value=0.0, max_value=1.0,
+                             value=0.9, step=0.01)
+
+        x_a = np.linspace(last_record[0]-49, last_record[0]+50, 100)
+        y_a = scs.binom(sum(last_record), c_a).pmf(x_a)
+        x_b = np.linspace(last_record[1]-49, last_record[1]+50, 100)
+        y_b = scs.binom(sum(last_record), c_b).pmf(x_b)
+
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=x_a, y=y_a,
+                                 mode='lines',
+                                 name='Control Group'))
+        fig.add_trace(go.Scatter(x=x_b, y=y_b,
+                                 mode='lines',
+                                 name='Treatment Group'))
+        fig.update_layout(
+            title_text="Binomial Distribution Representation of Control and Treatment Groups")
+        fig.update_xaxes(title="Count of Possible Outcomes")
+        fig.update_yaxes(title="Probability")
+        fig.update_layout(
+            autosize=False,
+            width=700,
+            height=500,
+        )
+        st.plotly_chart(fig)
+
+        st.write("Conversion Rate for Compartmental Modelling: ",
+                 round(c_a*100, 3),  "%")
+        st.write("Conversion Rate for Agent Based Modelling: ",
+                 round(c_b*100, 3),  "%")
+
+        st.write("Relative Uplift: ", round(cr_uplift*100, 3),  "%")
+        st.write("Z Score: ", round(z_score, 3))
+        st.write("P Value: ", round(p_value, 3))
+
+        if ((p_value < (1 - interval) and sided == 0) or ((p_value > (interval + (1 - interval)/2) or p_value < (1 - interval - (1 - interval)/2)) and sided == 1)):
+            st.write("Statistically significant: ", True)
+        else:
+            st.write("Statistically significant: ", False)
